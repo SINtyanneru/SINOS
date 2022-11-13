439fe5060e7d3ab4a8cc50315ea1bb1e553bbe3c
るみさん <63392249+SINtyanneru@users.noreply.github.com>
2022-11-13 15:05:16 +0900

    三目並べ5

--- ETC/3MOKU/Main.js
+++ ETC/3MOKU/Main.js
@@ -7,14 +7,21 @@ function sanmoki_Start(){
     const Window_element = Window_Create("三目並べ", 1, 0, 0, 500, 500);
     const WIndowID = Window_element.ID;
 
-    Window_Contents("<BUTTON class='sanmoku_0' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_1' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_2' onclick='sanmoku_Clicked(event)'>無</BUTTON><BR>" + 
-    "<BUTTON class='sanmoku_4' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_5' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_6' onclick='sanmoku_Clicked(event)'>無</BUTTON><BR>"+
-    "<BUTTON class='sanmoku_7' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_8' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_9' onclick='sanmoku_Clicked(event)'>無</BUTTON>",0,WIndowID);
+    Window_Contents("<DIV id='sanmoku_" + WIndowID + "'>丸</DIV>"+
+    "<BUTTON class='sanmoku_0_" + WIndowID + "' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_1_" + WIndowID + "' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_2_" + WIndowID + "' onclick='sanmoku_Clicked(event)'>無</BUTTON><BR>" + 
+    "<BUTTON class='sanmoku_3_" + WIndowID + "' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_4_" + WIndowID + "' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_5_" + WIndowID + "' onclick='sanmoku_Clicked(event)'>無</BUTTON><BR>"+
+    "<BUTTON class='sanmoku_6_" + WIndowID + "' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_7_" + WIndowID + "' onclick='sanmoku_Clicked(event)'>無</BUTTON><BUTTON class='sanmoku_8_" + WIndowID + "' onclick='sanmoku_Clicked(event)'>無</BUTTON>",0,WIndowID);
 }
 
 function sanmoku_Clicked(e){
-    e.target.innerHTML = "丸";
 
-    console.log(document.getElementsByClassName("sanmoku_" + ++e.target.className.split("_")[1]))
-    document.getElementsByClassName("sanmoku_" + ++e.target.className.split("_")[1])[0].innerHTML = "罰"
+    if(document.getElementById("sanmoku_" + e.target.className.split("_")[2]).innerHTML == "丸"){
+        e.target.innerHTML = "丸";
+        document.getElementById("sanmoku_" + e.target.className.split("_")[2]).innerHTML = "罰";
+    }else{
+        e.target.innerHTML = "罰";
+        document.getElementById("sanmoku_" + e.target.className.split("_")[2]).innerHTML = "丸";
+    }
+
+    //console.log(document.getElementsByClassName("sanmoku_" + ++e.target.className.split("_")[1]))
 }
