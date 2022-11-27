22b7aad1af88304b7d4341f42875216bbe28d194
SINtyanneru <irjitgd@gmail.com>
2022-11-27 13:29:44 +0900

    HYUMUNOSU

--- ETC/TARMINAL/Main.js
+++ ETC/TARMINAL/Main.js
@@ -2,46 +2,36 @@
  * ターミナル
  */
 
-var TARMINAL_TEXT = [];
 
 var TARMINAL_Version = "1.0";
 function TARMINAL_Start(){
 	//ウィンドウを作成
 	const Window_element = Window_Create("ターミナルちゃん " + TARMINAL_Version, 0, 0, 0, 500, 500);
 	//上のやつを描画
-	Window_Contents("<TEXTAREA id=\"TARMINAL_TEXTAREA_" + Window_element.ID + "\" style=\"width: 100%; height: calc(100% - 35px); resize: none;\" onkeydown=\"TARMINAL_TEXTINPUT(event, '" + Window_element.ID + "')\">SINOS Term" + TARMINAL_Version + "\n</TEXTAREA>",0,Window_element.ID);
+	Window_Contents("<TEXTAREA class=\"hm\" id=\"TARMINAL_TEXTAREA_" + Window_element.ID + "\" style=\"width: 100%; font-size: 50px; height: calc(100% - 100px); resize: none;\">SINOS Term" + TARMINAL_Version + "\n</TEXTAREA><INPUT class=\"hm\" id=\"TERMINAL_INPUT_" + Window_element.ID + "\" style=\"font-size: 50px;\" onkeydown=\"TARMINAL_TEXTINPUT(event, '" + Window_element.ID + "')\">",0,Window_element.ID);
 	document.getElementById("TARMINAL_TEXTAREA_" + Window_element.ID).value += "\nSINOS>"
-	TARMINAL_TEXT.push({"WINDOWID":Window_element.ID,"TEXT":""});
 }
 
 function TARMINAL_TEXTINPUT(e,WINDOWID){
 	//何個目のコンソールかを識別するためのカウント
 	var count = 0;
 	//ﾌｫｰｴｯﾁを回すのだ
-	TARMINAL_TEXT.forEach(element => {
-		//ウィンドウIDが一致したら
-		if(element.WINDOWID == WINDOWID){
-			var TARMINAL_TEXTAREA = document.getElementById("TARMINAL_TEXTAREA_" + WINDOWID)
-			//キーはエンターキー？
-			if(e.key == "Enter"){
-				//コマンド処理
-				if(TARMINAL_TEXT[count].TEXT == "tanzania"){
-					TARMINAL_TEXTAREA.value += "\nTANZANIA!!!\n";
-					const music = new Audio('./ETC/TARMINAL/TANZANIA.mp3');
+	var TARMINAL_TEXTAREA = document.getElementById("TARMINAL_TEXTAREA_" + WINDOWID);
+	var TARMINAL_INPUT = document.getElementById("TERMINAL_INPUT_" + WINDOWID);
+	//キーはエンターキー？
+	if(e.key == "Enter"){
+		TARMINAL_TEXTAREA.value += "\n>" + TARMINAL_INPUT.value + "\n";
 
-					music.play();
-				}
-
-				TARMINAL_TEXTAREA.value += "\nSINOS>"
-				//内容をリセット
-				TARMINAL_TEXT[count].TEXT = "";
-			}else{
-				//エンターキーじゃないので、内容を+する
-				TARMINAL_TEXT[count].TEXT += e.key;
-			}
+		//コマンド処理
+		if(TARMINAL_INPUT.value == "tanzania"){
+			TARMINAL_TEXTAREA.value += "\nTANZANIA!!!\n";
+			const music = new Audio('./ETC/TARMINAL/TANZANIA.mp3');
+			music.play();
 		}
 
-		//カウントに+1
-		count++;
-	});
+		TARMINAL_TEXTAREA.value += "\nOK";
+		//内容をリセット
+		TARMINAL_INPUT.value = "";
+		TARMINAL_TEXTAREA.scrollTo(0, TARMINAL_TEXTAREA.scrollHeight);
+	}
 }
